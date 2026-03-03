80a073d26029fbc50dc0747b8d94806aee93a290
poojitha-arch <poojitha27.ai@gmail.com>
2026-03-03 16:37:39 +0530

    Added exp5  files

---
+++ src/main/java/com/klu/model/Student.java
@@ -0,0 +1,38 @@
+package com.klu.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+
+    private int id;
+    private String name;
+    private String gender;
+
+    @Autowired
+    private Certification certification;
+
+    public Student() {
+        this.id = 101;
+        this.name = "Indhu";
+        this.gender = "Female";
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Certification getCertification() {
+        return certification;
+    }
+}
