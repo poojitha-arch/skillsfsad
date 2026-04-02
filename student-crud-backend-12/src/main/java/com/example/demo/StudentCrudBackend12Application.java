7442515e57bef5c7c007433ace91d83be399d999
poojitha-arch <poojitha27.ai@gmail.com>
2026-04-02 14:32:05 +0530

    Added entire folder

---
+++ student-crud-backend-12/src/main/java/com/example/demo/StudentCrudBackend12Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCrudBackend12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCrudBackend12Application.class, args);
+	}
+
+}
